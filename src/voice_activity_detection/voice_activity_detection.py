917fe8178d668c6c9291fbf7011391b52ca8813b
Tianren Li <tianrenli92@gmail.com>
2018-06-29 11:32:40 -0400

    recalc features

--- src/voice_activity_detection/voice_activity_detection.py
+++ src/voice_activity_detection/voice_activity_detection.py
@@ -7,28 +7,22 @@ Created on Mon Feb 19 10:12:18 2018
 """
 
 # creating dataset for voice acitivity detection
-from .extract_features import extract_features
+from extract_features import extract_features
 import numpy as np
 import os
 from os.path import dirname,abspath,join
 import scipy.io.wavfile as wav
-
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import copy
-# import random
-# from scipy.stats import mode
+import pandas as pd
+import pickle
 
 
 DATA_FOLDER = join(dirname(dirname(abspath(__file__))),'data','noise-train')
 FRAME_LENGTH = 25  # ms
-WINDOW_LENGTH = 5  # s
+WINDOW_LENGTH = 1  # s
 
+feature_name = "RMS,SE,ZCR,LEFR,SF,SRF,SC,BW,NWPD,RSE,type,name,number".split(",")
+features_dict = {feature:[] for feature in feature_name}
 
-os.chdir(DATA_FOLDER)
-dataset_dict = {"ZCR": [], "RMS": [], "spectral_flux": [], \
-                "spectral_centroid": [], "spectral_rolloff": [], \
-                "bandwidth": [], "audio": [], "nwpd": [], "rse": []}
 for root, dirs, files in os.walk(DATA_FOLDER):
     for audio in files:
         if "noise" in audio or "music" in audio or "speech" in audio or "audio" in audio:
@@ -37,25 +31,28 @@ for root, dirs, files in os.walk(DATA_FOLDER):
             sampling_rate, sig = wav.read(join(root, audio))
             print("sampling rate:", sampling_rate, "signal length", len(sig))
             index = 0
+            number = 0
             while index + (sampling_rate * WINDOW_LENGTH) < len(sig):
                 sample = sig[index:(index + (sampling_rate * WINDOW_LENGTH))]
                 ef = extract_features(sample, FRAME_LENGTH, sampling_rate)
-                ZCR, RMS, sf, sr, sc, bd, nwpd, rse = ef.return_()
-                dataset_dict["ZCR"].append(ZCR)
-                dataset_dict["RMS"].append(RMS)
-                dataset_dict["spectral_flux"].append(np.mean(sf))
-                dataset_dict["spectral_centroid"].append(np.mean(sc))
-                dataset_dict["spectral_rolloff"].append(np.mean(sr))
-                dataset_dict["bandwidth"].append(np.mean(bd))
-                dataset_dict["nwpd"].append(np.mean(nwpd))
-                dataset_dict["rse"].append(np.mean(rse))
-                dataset_dict["audio"].append(audio.split("-")[0])
+                rms, se, zcr, lefr, sf, srf, sc, bd, nwpd, rse = ef.return_()
+                features_dict["RMS"].append(rms)
+                features_dict["SE"].append(se)
+                features_dict["ZCR"].append(zcr)
+                features_dict["LEFR"].append(lefr)
+                features_dict["SF"].append(np.mean(sf))
+                features_dict["SC"].append(np.mean(sc))
+                features_dict["SRF"].append(np.mean(srf))
+                features_dict["BW"].append(np.mean(bd))
+                features_dict["NWPD"].append(np.mean(nwpd))
+                features_dict["RSE"].append(np.mean(rse))
+                features_dict["type"].append(audio.split("-")[0])
+                features_dict["name"].append(audio)
+                features_dict["number"].append(number)
+                number+=1
                 index += sampling_rate * WINDOW_LENGTH
-                # if index == 5*sampling_rate*WINDOW_LENGTH:
-                #    break
-values = dataset_dict.values()
-print([len(e) for e in values])
-print("finished")
-np.save(join(DATA_FOLDER,"features.npy"), dataset_dict)
 
-
+features_df=pd.DataFrame.from_dict(features_dict)
+features_df = features_df[feature_name]
+with open(join(DATA_FOLDER, "features_df_1s.pickle"), "wb") as file:
+    pickle.dump(features_df, file)
